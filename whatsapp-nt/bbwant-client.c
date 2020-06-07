12a22006d1db9594833f528e99d046fd93370ff4
Janne Paalijarvi <jpaalija@gmail.com>
2020-06-08 00:37:05 +0300

    System is able to produce placeholder binary printing its own name and version! Small step for humanity, big step for me.

---
+++ whatsapp-nt/bbwant-client.c
@@ -0,0 +1,17 @@
+#include "bbwant-client.h"
+
+#include <stdio.h>
+
+
+void BBWANT_Client_PrintHelp(void)
+{
+  printf("bitlbee-whatsapp-nt-client v. 0.0.1\n");
+}
+
+
+int main(int argc, char* argv)
+{
+  BBWANT_Client_PrintHelp();
+
+  return 0;
+}
---
+++ whatsapp-nt/bbwant-client.h
@@ -0,0 +1,6 @@
+#ifndef _BBWANT_CLIENT_H_
+#define _BBWANT_CLIENT_H_
+
+
+
+#endif // #ifndef _BBWANT_CLIENT_H_
