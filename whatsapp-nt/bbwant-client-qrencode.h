54acd6a0487761c800c3e5bb825c96beb08abcc0
Janne Paalijarvi <jpaalija@gmail.com>
2020-06-08 01:42:20 +0300

    Adding QR PNG encoding from another project

---
+++ whatsapp-nt/bbwant-client-qrencode.h
@@ -0,0 +1,10 @@
+#ifndef _BBWANT_CLIENT_QRENCODE_H_
+#define _BBWANT_CLIENT_QRENCODE_H_
+
+#include <qrencode.h>
+
+
+int iBBWANT_Client_WriteQRAsPNG(QRcode *qrcode, const char *outfile);
+
+
+#endif // #ifdef _BBWANT_CLIENT_QRENCODE_H_
